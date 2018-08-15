1c40df8f716dec8743965ebcb409f618b34ea4bc
TKais <TiffanyTKaiser@gmail.com>
2018-08-14 21:54:48 -0700

    refactor: Refactor the App component to include the NavMenu and routing to the SignIn component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoadingBar from 'react-redux-loading';
+import NavMenu from './NavMenu';
+import Home from './Home';
+import SignIn from './SignIn';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello!</h1>
-      </div>
+      <Router>
+        <Fragment>
+          <div className='main-app'>
+            <NavMenu />
+            <Route path='/' exact component={SignIn} />
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
+
+// function mapStateToProps ({ authedUser }) {
+//   return {
+//     loading: authedUser === null
+//   }
+// }
 
 export default App;
