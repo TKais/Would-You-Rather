9d23127805eb846764b43fd9cb05eee53918a543
TKais <TiffanyTKaiser@gmail.com>
2018-08-14 21:52:17 -0700

    feat: Implement a SignIn component
    This component will be a sign-in form

---
+++ src/components/SignIn.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+// TODO: Get data passed down from App.js
+const names = [{ name: 'tiff'}, {name: 'ale'}, { name: 'inna'}];
+
+class SignIn extends React.Component {
+
+	state = {
+		val: ''
+	}
+
+	handleChange = (e) => {
+		const value = e.target.value;
+		this.setState({ val: value });
+	}
+
+	render() {
+		return (
+			<div className="signin">
+				<Paper elevation={1}>
+				  <Typography variant="headline" component="h3" className="signin__header">
+		            Sign In
+		          </Typography>
+		          <TextField
+			          id="select-username"
+			          select
+			          className="signin__select"
+			          label="Select Username"
+			          value={this.state.val}
+			          onChange={this.handleChange}
+			          margin="normal"
+			      >
+			          {names.map( option => (
+				          <MenuItem key={option.name} value={option.name}>
+			                {option.name}
+			              </MenuItem>
+		              ))}
+	              </TextField>
+				</Paper>
+			</div>
+		);
+	}
+}
+
+export default SignIn;
