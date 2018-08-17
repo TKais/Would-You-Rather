9fdf7f490c9f6ed2ff29e1d0e9f70826591b9988
TKais <TiffanyTKaiser@gmail.com>
2018-08-16 21:22:59 -0700

    feat: Add an index file to combine reducers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import reducer from './reducers';
+import reducer from './reducers';
 // import middleware from './middleware';
 
-// const store = createStore(reducer, middleware);
+const store = createStore(reducer);
 
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import currentUser from './currentUser';
+import { loadingBarReducer } from 'react-redux-loading';
+
+export default combineReducers({
+	currentUser,
+	loader: loadingBarReducer
+});
