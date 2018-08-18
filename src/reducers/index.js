a0ed15b47990cffdbcff983a9ef320fd0ff9496f
TKais <TiffanyTKaiser@gmail.com>
2018-08-17 20:53:51 -0700

    feat: Add a new reducer for retrieving all users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { currentUser } from './currentUser';
+import { allUsers } from './users';
 import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
 	currentUser,
+	allUsers,
 	loader: loadingBarReducer
 });
