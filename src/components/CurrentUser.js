166fe9d17c27af004d9f56a45cf12b155250c6b2
TKais <TiffanyTKaiser@gmail.com>
2018-08-19 16:23:38 -0700

    feat: Create a component for tracking the current user on the screen at all times

---
+++ src/components/CurrentUser.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class CurrentUser extends React.Component {
+	getCurrentUser() {
+		const user = this.props.currentUser;
+		const loggedInUser = user !== null && Object.values(user).join('');
+		const name = loggedInUser && this.props.users[loggedInUser].name;
+
+		if( name ) {
+			return `Welcome, ${name}!`;
+		} else {
+			return '';
+		}
+	}
+
+	render() {
+		return (
+			<div>{ this.getCurrentUser() }</div>
+		);
+	}
+}
+
+function mapStateToProps({ currentUser, users }) {
+	return {
+		currentUser,
+		users,
+	}
+}
+
+export default connect(mapStateToProps)(CurrentUser)
