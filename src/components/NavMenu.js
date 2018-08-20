f7251e30c628150e21b8ec56fe1059e591887f31
TKais <TiffanyTKaiser@gmail.com>
2018-08-19 18:15:04 -0700

    refactor: Only show logout link in NavMenu if user is logged in

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 
-function NavMenu() {
-	return (
-		<nav className="navmenu">
-		    <ul className="navmenu__list">
-		        <li><NavLink to='/' exact activeClassName='active'>Home</NavLink></li>
-		        <li><NavLink to='/new' activeClassName='active'>New Question</NavLink></li>
-		        <li><NavLink to='/leaderboard' activeClassName='active'>Leaderboard</NavLink></li>
-		        <li><NavLink to='/' exact activeClassName='active'>Logout</NavLink></li>
-		    </ul>
-		</nav>
-	);
+class NavMenu extends React.Component {
+	generateLogOutLink() {
+		if( this.props.currentUser !== null ) {
+			return <li><NavLink to='/' exact activeClassName='active'>Logout</NavLink></li>;
+		}
+	}
+
+	render() {
+		return (
+			<nav className="navmenu">
+			    <ul className="navmenu__list">
+			        <li><NavLink to='/' exact activeClassName='active'>Home</NavLink></li>
+			        <li><NavLink to='/new' activeClassName='active'>New Question</NavLink></li>
+			        <li><NavLink to='/leaderboard' activeClassName='active'>Leaderboard</NavLink></li>
+			        { this.generateLogOutLink() }
+			    </ul>
+			</nav>
+		);
+	}
 }
 
-export default NavMenu;
+function mapStateToProps({ currentUser }) {
+	return {
+		currentUser
+	}
+}
+
+export default connect(mapStateToProps)(NavMenu);
