21bbdd252612c8ca6642a415d12fed089ea61461
TKais <TiffanyTKaiser@gmail.com>
2018-08-21 19:25:19 -0700

    feat: Don't let links navigate to pages if the user is not logged in

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { setCurrentUser } from '../actions/currentUser';
 
 
 class NavMenu extends React.Component {
+	constructor() {
+		super();
+		this.handleClick = this.handleClick.bind(this);
+	}
+
 	generateLogOutLink() {
 		if( this.props.currentUser !== null ) {
-			return <li><NavLink to='/' exact activeClassName='active'>Logout</NavLink></li>;
+			return <li><NavLink to='/' exact activeClassName='active logout'>Logout</NavLink></li>;
+		}
+	}
+
+	currentUserIsDefined( route ) {
+		if( this.props.currentUser !== null ) {
+			return route;
+		} else {
+			return '/';
+		}
+	}
+
+	handleClick( event ) {
+		const classes = Array.from(event.target.classList);
+		if(classes.indexOf('logout') > -1) {
+			this.props.dispatch(setCurrentUser(null));
 		}
 	}
 
 	render() {
 		return (
 			<nav className="navmenu">
-			    <ul className="navmenu__list">
-			        <li><NavLink to='/' exact activeClassName='active'>Home</NavLink></li>
-			        <li><NavLink to='/new' activeClassName='active'>New Question</NavLink></li>
-			        <li><NavLink to='/leaderboard' activeClassName='active'>Leaderboard</NavLink></li>
+			    <ul className="navmenu__list" onClick={this.handleClick}>
+			        <li><NavLink to={this.currentUserIsDefined('/')} exact activeClassName='active home'>Home</NavLink></li>
+			        <li><NavLink to={this.currentUserIsDefined('/new')} activeClassName='active new'>New Question</NavLink></li>
+			        <li><NavLink to={this.currentUserIsDefined('/leaderboard')} activeClassName='active leaderboard'>Leaderboard</NavLink></li>
 			        { this.generateLogOutLink() }
 			    </ul>
 			</nav>
