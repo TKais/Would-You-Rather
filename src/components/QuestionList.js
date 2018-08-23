69e398a14d44318b17f8d97fa9ac553d5bede89e
TKais <TiffanyTKaiser@gmail.com>
2018-08-22 21:36:02 -0700

    feat: Start adding functionality for displaying questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class QuestionList extends React.Component {
+	getRoute = (question) => {
+		return `questions/${question}`;
+	}
+
 	render() {
 		return (
-			<div>Question</div>
+			<div className="questionlist">
+			    <ul className="questionlist__menu">
+			        { Object.keys(this.props.questions).map( (question) => (
+			        	<li className="questionlist__menu-item" key={this.props.questions[question].id}>
+			        	    <NavLink to={this.getRoute(question)} exact activeClassName='active'>{this.props.questions[question].id}</NavLink>
+			        	</li>
+			        ))}
+			    </ul>
+			</div>
 		)
 	}
 }
 
-export default connect()(QuestionList)
+function mapStateToProps({ questions }) {
+	return {
+		questions
+	}
+}
+
+export default connect(mapStateToProps)(QuestionList)
