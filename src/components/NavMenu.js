a07bb455b3f6049e8e11bd099a2aea9afef7653d
TKais <TiffanyTKaiser@gmail.com>
2018-08-22 20:17:53 -0700

    feat: Build an error message when a user attempts to navigate the app before signing in

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { setCurrentUser } from '../actions/currentUser';
+import Snackbar from '@material-ui/core/Snackbar';
 
 
 class NavMenu extends React.Component {
+	state = {
+		showError : false
+	}
+
 	constructor() {
 		super();
 		this.handleClick = this.handleClick.bind(this);
+		this.handleState = this.handleState.bind(this);
+		this.handleErrorMessageClose = this.handleErrorMessageClose.bind(this);
 	}
 
 	generateLogOutLink() {
@@ -29,6 +36,21 @@ class NavMenu extends React.Component {
 		if(classes.indexOf('logout') > -1) {
 			this.props.dispatch(setCurrentUser(null));
 		}
+
+		this.handleState();
+	}
+
+	handleState() {
+		const userState = this.props.currentUser === null;
+		this.setState({ showError : userState });
+	}
+
+	handleErrorMessageClose(event, reason) {
+	    if (reason === 'clickaway') {
+	      return;
+	    }
+
+	    this.setState({ showError: false });
 	}
 
 	render() {
@@ -40,6 +62,19 @@ class NavMenu extends React.Component {
 			        <li><NavLink to={this.currentUserIsDefined('/leaderboard')} activeClassName='active leaderboard'>Leaderboard</NavLink></li>
 			        { this.generateLogOutLink() }
 			    </ul>
+			    <Snackbar
+		          anchorOrigin={{
+		            vertical: 'top',
+		            horizontal: 'center',
+		          }}
+		          open={this.state.showError}
+		          autoHideDuration={3000}
+		          onClose={this.handleErrorMessageClose}
+		          ContentProps={{
+		            'aria-describedby': 'message-id',
+		          }}
+		          message={<span id="message-id">Please sign in before continuing</span>}
+		        />
 			</nav>
 		);
 	}
