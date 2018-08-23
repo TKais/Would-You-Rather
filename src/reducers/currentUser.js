76228820c5c5828c415e9a3c8c1d4d084c60e262
TKais <TiffanyTKaiser@gmail.com>
2018-08-22 19:28:19 -0700

    fix: Make sure the currentUser reducer handles logging out by returning null

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -3,9 +3,13 @@ import { SET_CURRENT_USER } from '../actions/currentUser';
 export function currentUser(state = null, action) {
 	switch(action.type) {
 		case SET_CURRENT_USER:
-		    return {
-		    	...state,
-		    	...action.id
+		    if( action.id !== null ) {
+		    	return {
+		            ...state,
+		            ...action.id
+		    	}
+		    } else {
+		    	return null;
 		    }
 		default:
 		    return state
