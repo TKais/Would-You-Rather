6e5b50ad78379796c032c5741e6b262b0abf8b60
TKais <TiffanyTKaiser@gmail.com>
2018-08-22 21:54:16 -0700

    refactor: Have QuestionList render a Question component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import Question from './Question';
 
 class QuestionList extends React.Component {
-	getRoute = (question) => {
-		return `questions/${question}`;
-	}
-
 	render() {
 		return (
 			<div className="questionlist">
 			    <ul className="questionlist__menu">
 			        { Object.keys(this.props.questions).map( (question) => (
-			        	<li className="questionlist__menu-item" key={this.props.questions[question].id}>
-			        	    <NavLink to={this.getRoute(question)} exact activeClassName='active'>{this.props.questions[question].id}</NavLink>
-			        	</li>
+			        	<Question question={this.props.questions[question]} allQuestions={this.props.questions} listKey={this.props.questions[question].id} />
 			        ))}
 			    </ul>
 			</div>
