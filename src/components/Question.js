2a36ea98513727a2b62fea4bc13a1df55c397d05
TKais <TiffanyTKaiser@gmail.com>
2018-08-22 21:56:57 -0700

    feat: Add a Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Question extends React.Component {
+	getRoute = (question) => {
+		return `questions/${question.id}`;
+	}
+
 	render() {
 		return (
-			<div>A single question</div>
+			<li className="questionlist__menu-item" key={this.props.listKey}>
+    	        <NavLink to={this.getRoute(this.props.question)} exact activeClassName='active'>{this.props.question.id}</NavLink>
+        	</li>
 		);
 	}
 }
