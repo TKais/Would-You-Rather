377f034709580520a251bf2bebf448ae11866455
TKais <TiffanyTKaiser@gmail.com>
2018-08-23 19:14:57 -0700

    refactor: Don't pass all questions down as props

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -8,7 +8,7 @@ class QuestionList extends React.Component {
 			<div className="questionlist">
 			    <ul className="questionlist__menu">
 			        { Object.keys(this.props.questions).map( (question) => (
-			        	<Question question={this.props.questions[question]} allQuestions={this.props.questions} listKey={this.props.questions[question].id} />
+			        	<Question question={this.props.questions[question]} listKey={this.props.questions[question].id} />
 			        ))}
 			    </ul>
 			</div>
