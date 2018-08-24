37e5b6ae87e3b36e8b4aec2cace865f3d026cd3b
TKais <TiffanyTKaiser@gmail.com>
2018-08-23 19:15:56 -0700

    feat: Add a method to format the names of the question authors

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Question extends React.Component {
 	getRoute = (question) => {
 		return `questions/${question.id}`;
 	}
 
+	formatName = (question) => {
+		const author = question.author;
+		const formattedAuthor = this.props.users[author].name;
+
+		return formattedAuthor;
+	}
+
 	render() {
 		return (
 			<li className="questionlist__menu-item" key={this.props.listKey}>
-    	        <NavLink to={this.getRoute(this.props.question)} exact activeClassName='active'>{this.props.question.id}</NavLink>
+			    <p>{ `${this.formatName(this.props.question)} asks...` }</p>
+    	        <NavLink to={this.getRoute(this.props.question)} exact className='questionlist__menu-item-anchor'>{this.props.question.id}</NavLink>
         	</li>
 		);
 	}
 }
 
-export default Question;
+function mapStateToProps({ users, questions }) {
+	return {
+		users,
+		questions,
+	}
+}
+
+export default connect(mapStateToProps)(Question)
