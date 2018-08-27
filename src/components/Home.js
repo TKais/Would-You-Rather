9a02ad736de7ad8c1ee7e75e92461c0007f31b63
TKais <TiffanyTKaiser@gmail.com>
2018-08-26 23:05:38 -0700

    refactor: Split questions up into answered and unanswered

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,50 @@ import { connect } from 'react-redux';
 import QuestionList from './QuestionList';
 
 class Home extends React.Component {
+	state = {
+		unanswered: {},
+		answered: {},
+	}
+
+	componentDidMount() {
+		const user = this.props.users[this.props.currentUser];
+		const answered = Object.keys(user.answers).map( (answer) => {
+			return this.props.questions[answer];
+		});
+		const unanswered = Object.keys(this.props.questions).map( (question) => {
+			if( !(user.answers.hasOwnProperty(question)) ) {
+				return this.props.questions[question];
+			} else {
+				return null;
+			}
+		});
+		this.setState({
+			answered,
+			unanswered
+		});
+	}
+
 	render() {
 		return (
 			<div>
 			    <div>
 				    <h3>Unanswered Questions</h3>
-					<QuestionList questions={this.props.unanswered} />
+					<QuestionList questions={this.state.unanswered} />
 				</div>
 				<div>
 					<h3>Answered Questions</h3>
-					<QuestionList questions={this.props.answered} />
+					<QuestionList questions={this.state.answered} />
 				</div>
 			</div>
 		);
 	}
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, currentUser, users }) {
 	return {
-		answered: questions,
-		unanswered: questions,
+		currentUser: Object.values(currentUser).join(''),
+		questions,
+		users,
 	}
 }
 
