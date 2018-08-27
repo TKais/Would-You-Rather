9b63b74b080399e88849ab146756147da6b9c7dd
TKais <TiffanyTKaiser@gmail.com>
2018-08-26 21:40:32 -0700

    refactor: Make QuestionList a stateless functional component that accepts questions as props

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Question from './Question';
 
-class QuestionList extends React.Component {
-	render() {
-		return (
-			<div className="questionlist">
-			    <ul className="questionlist__menu">
-			        { Object.keys(this.props.questions).map( (question) => (
-			        	<Question question={this.props.questions[question]} listKey={this.props.questions[question].id} />
-			        ))}
-			    </ul>
-			</div>
-		)
-	}
+function QuestionList(props){
+	console.log('PROPS--------------->>>', props);
+	return (
+		<div className="questionlist">
+		    <ul className="questionlist__menu">
+		        { props.questions ? 
+		            Object.keys(props.questions).map( (question) => (
+		        	<Question question={props.questions[question]} listKey={props.questions[question].id} />
+		          )) :
+		          null
+		        }
+		    </ul>
+		</div>
+	)
 }
 
-function mapStateToProps({ questions }) {
-	return {
-		questions
-	}
-}
-
-export default connect(mapStateToProps)(QuestionList)
+export default QuestionList;
