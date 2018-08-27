3488e681c759055b4d4a98e060aa01f674d6a5c5
TKais <TiffanyTKaiser@gmail.com>
2018-08-26 23:06:37 -0700

    refactor: Accommodate null values

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Question from './Question';
 
 function QuestionList(props){
-	console.log('PROPS--------------->>>', props);
 	return (
 		<div className="questionlist">
 		    <ul className="questionlist__menu">
 		        { props.questions ? 
-		            Object.keys(props.questions).map( (question) => (
-		        	<Question question={props.questions[question]} listKey={props.questions[question].id} />
-		          )) :
+		            Object.keys(props.questions).map( (question) => {
+		            	if( props.questions[question] !== null ) {
+				        	return (
+				        		<Question question={props.questions[question]} listKey={props.questions[question].id} />
+			        		);
+				        }
+		          }) :
 		          null
 		        }
 		    </ul>
