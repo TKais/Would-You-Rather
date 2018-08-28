a6bbf98c7363416570170b9d159f791f953a8b21
TKais <TiffanyTKaiser@gmail.com>
2018-08-27 20:07:48 -0700

    refactor: Import new CSS file for NavMenu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import '../assets/css/navmenu.css';
 import { setCurrentUser } from '../actions/currentUser';
 import Snackbar from '@material-ui/core/Snackbar';
 
