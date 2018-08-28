cebd6cf80334a08be8e8330a89a7377a0a6125fc
TKais <TiffanyTKaiser@gmail.com>
2018-08-27 21:34:04 -0700

    feat: Add dispatch for actions

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import '../assets/css/newquestion.css';
+import Button from '@material-ui/core/Button';
+import { createQuestion } from '../actions/questions';
 
 class NewQuestion extends React.Component {
 	state = {
@@ -9,8 +11,19 @@ class NewQuestion extends React.Component {
 		optionTwoValue: '',
 	}
 
-	handleSubmit (event) {
-		console.log('EVENT--->', event);
+	handleSubmit = (event) => {
+		event.preventDefault();
+		const { optionOneValue, optionTwoValue } = this.state;
+		const question = { optionOneValue, optionTwoValue, author: this.props.currentUser };
+		this.props.dispatch(createQuestion(question));
+	}
+
+	handleChange = (event) => {
+		const value = event.target.value;
+		const id = event.target.id;
+		const inputToUpdate = id === 'option-one' ? 'optionOneValue' : 'optionTwoValue';
+
+		this.setState({ [inputToUpdate]: value });
 	}
 
 	render() {
@@ -18,14 +31,27 @@ class NewQuestion extends React.Component {
 			<div className="new-question">
 			    <h2>Create New Question</h2>
 				<form className="new-question__form" onSubmit={ this.handleSubmit }>
-					<label for="option-one">Option One</label>
-					<input id="option-one" type="text" value={ this.state.optionOneValue } />
-					<label for="option-two">Option Two</label>
-					<input id="option-two" type="text" value={ this.state.optionTwoValue } />
+					<label htmlFor="option-one">Option One</label>
+					<input id="option-one" type="text" value={ this.state.optionOneValue } onChange={ this.handleChange } />
+					<label htmlFor="option-two">Option Two</label>
+					<input id="option-two" type="text" value={ this.state.optionTwoValue } onChange={ this.handleChange } />
+					<Button
+				        variant="contained"
+				        type="submit"
+				        color="primary"
+				        className="new-question__form-button"
+				        onClick={this.handleClick}
+				    >Submit</Button>
 				</form>
 			</div>
 		)
 	}
 }
 
-export default connect()(NewQuestion);
+function mapStateToProps({ currentUser }) {
+	return {
+		currentUser: Object.values(currentUser).join('')
+	}
+}
+
+export default connect(mapStateToProps)(NewQuestion);
