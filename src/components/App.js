1e93a8ddf6385a81d04a6e00d8b6e0f6c6ebbd75
TKais <TiffanyTKaiser@gmail.com>
2018-08-27 20:05:31 -0700

    refactor: Import new CSS file for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import '../assets/css/app.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { getAllData } from '../actions/shared';
 import { LoadingBar } from 'react-redux-loading';
