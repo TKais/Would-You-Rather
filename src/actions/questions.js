fdd2e3bcaf9cb0b8808612c63e5633ad66372355
TKais <TiffanyTKaiser@gmail.com>
2018-08-27 21:33:15 -0700

    feat: Add actions for submitting a question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,27 @@
+import { _saveQuestion } from '../data/_DATA';
 export const GET_QUESTIONS = 'GET_QUESTIONS';
+export const CREATE_QUESTION = 'CREATE_QUESTION';
 
 export function getQuestions(questions) {
 	return {
 		type: GET_QUESTIONS,
 		questions,
+	}
+}
+
+export function submitQuestion(question) {
+	return {
+		type: CREATE_QUESTION,
+		question
+	}
+}
+
+export function createQuestion(question) {
+	return (dispatch) => {
+		return _saveQuestion(question)
+		    .then( (data) => {
+		    	console.log('DATA???', data);
+		    	dispatch(submitQuestion(data));
+		    });
 	}
 }
