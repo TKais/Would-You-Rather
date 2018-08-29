058593132e5edd1da436134e0204e161919c0c0d
TKais <TiffanyTKaiser@gmail.com>
2018-08-28 22:52:05 -0700

    fix: Remove loading logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import { currentUser } from './currentUser';
 import { users } from './users';
 import { questions } from './questions';
-import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
 	currentUser,
 	users,
 	questions,
-	loader: loadingBarReducer
 });
