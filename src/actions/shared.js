e536fb355fb9714a8505afba116dc953a65753b1
TKais <TiffanyTKaiser@gmail.com>
2018-08-28 22:51:24 -0700

    fix: Remove loading logic

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,3 @@
-import { showLoading, hideLoading } from 'react-redux-loading';
 import * as UserActions from './users';
 import * as QuestionActions from './questions';
 import { getInitialData } from '../data/api';
@@ -6,7 +5,6 @@ import { getInitialData } from '../data/api';
 // Uses Thunk pattern for async data
 export function getAllData() {
 	return (dispatch) => {
-		dispatch(showLoading())
 		return getInitialData()
 		    .then( (data) => {
 		    	dispatch( UserActions.getUsers(data.users));
