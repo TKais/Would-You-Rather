532255b86e7f7b4ac6be3c2457c79343d362abbf
TKais <TiffanyTKaiser@gmail.com>
2018-08-28 22:52:43 -0700

    refactor: Make sure leaderboard cannot be accessed unless a user is logged in

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Leaderboard extends React.Component {
 	render() {
+		if (  this.props.currentUser === null ) {
+	      return <Redirect to='/' />
+	    }
+
 		return (
 			<div>Leaderboard</div>
 		);
 	}
 }
 
-export default Leaderboard;
+function mapStateToProps({ currentUser }) {
+	return {
+		currentUser: currentUser ? Object.values(currentUser).join('') : null
+	}
+}
+
+export default connect(mapStateToProps)(Leaderboard);
