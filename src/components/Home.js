876c98e60d8b5da181b2d51911c7bc15f65e9c4f
TKais <TiffanyTKaiser@gmail.com>
2018-08-29 20:57:38 -0700

    refactor: Change the way classes are named in the Home component / use local state to set whether questions are hidden or shown

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import QuestionList from './QuestionList';
+import Button from '@material-ui/core/Button';
 import '../assets/css/home.css';
 
 class Home extends React.Component {
+	state = {
+		hideAnswered: true,
+		hideUnanswered: false,
+	}
+
 	generateAnsweredQuestions = () => {
 		const user = this.props.users[this.props.currentUser];
 		const answered = Object.keys(user.answers).map( (answer) => {
@@ -26,15 +32,40 @@ class Home extends React.Component {
 		return unanswered;
 	}
 
+	handleClick = ( event ) => {
+		const target = event.target;
+		const classes = target.classList;
+
+		if( classes.contains( 'home-component__buttons--unanswered' ) ) {
+			this.setState({ hideAnswered: true, hideUnanswered: false });
+		} else if ( classes.contains( 'home-component__buttons--answered' ) ) {
+			this.setState({ hideAnswered: false, hideUnanswered: true });
+		}
+	}
+
 	render() {
 		return (
-			<div>
-			    <div>
-				    <h3>Unanswered Questions</h3>
+			<div className="home-component">
+			    <div className="home-component__buttons">
+				    <Button
+				        variant="contained"
+				        type="button"
+				        color="primary"
+				        className="home-component__buttons--unanswered"
+				        onClick={this.handleClick}
+				    >Unanswered Questions</Button>
+				    <Button
+				        variant="contained"
+				        type="button"
+				        color="primary"
+				        className="home-component__buttons--answered"
+				        onClick={this.handleClick}
+				    >Answered Questions</Button>
+			    </div>
+			    <div className={`home-component__question-unanswered-list ${ this.state.hideUnanswered ? 'home-component__question-hide' : '' }`}>
 					<QuestionList questions={ this.generateUnansweredQuestions() } />
 				</div>
-				<div>
-					<h3>Answered Questions</h3>
+				<div className={`home-component__question-answered-list ${ this.state.hideAnswered ? 'home-component__question-hide' : '' }`}>
 					<QuestionList questions={ this.generateAnsweredQuestions() } />
 				</div>
 			</div>
