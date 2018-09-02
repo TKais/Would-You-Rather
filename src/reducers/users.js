8f76059f3891c67afd22a249c244d2800449facb
TKais <TiffanyTKaiser@gmail.com>
2018-09-01 17:22:04 -0700

    feat: Update user reducer to handle adding a new question

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { GET_USERS } from '../actions/users';
+import { GET_USERS, ADD_USER_ANSWER } from '../actions/users';
 
 export function users(state = {}, action) {
 	switch(action.type) {
@@ -6,6 +6,23 @@ export function users(state = {}, action) {
 		    return {
 		    	...state,
 		    	...action.users,
+		    }
+		case ADD_USER_ANSWER:
+		    const { answer } = action;
+		    const answerObject = state[answer.authedUser].answers;
+		    const newAnswer = { [answer.qid]: answer.answer };
+		    let newAnswerState = {};
+		    if(!answerObject.hasOwnProperty(action.qid)) {
+		    	newAnswerState = {
+			    	[answer.authedUser]: {
+			    		...state[answer.authedUser],
+			    		answers: Object.assign({}, answerObject, newAnswer ),
+			    	}
+			    }
+		    }
+		    return {
+		    	...state,
+		    	...newAnswerState,
 		    }
 		default:
 		    return state
