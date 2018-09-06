6c3cf3977b18ecbf2aec10a19f1ae6396148c69b
TKais <TiffanyTKaiser@gmail.com>
2018-09-05 18:52:03 -0700

    feat: Plot data into the score card

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function ScoreCard(props) {
 	return (
-		<div>Score Card</div>
+		<Card>
+		    <CardContent>
+		        <h3>{props.user.name}</h3>
+		        <p>Answered Questions: {Object.keys(props.user.answers).length}</p>
+		        <p>Created Questions: {props.user.questions.length}</p>
+		        <p>Score: {Object.keys(props.user.answers).length + props.user.questions.length}</p>
+		        <img src={props.user.avatarURL}/>
+		    </CardContent>
+		</Card>
 	);
 }
 
