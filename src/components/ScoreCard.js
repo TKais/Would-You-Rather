2d1dd4c85f91fa5caebb3fe24d5cf61ef44b4d7c
TKais <TiffanyTKaiser@gmail.com>
2018-09-12 19:40:36 -0700

    fix: Refactor leaderboard to not include scorecard styles

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import '../assets/css/scorecard.css';
 
 function ScoreCard(props) {
 	return (
-		<Card>
+		<Card className="scorecard">
 		    <CardContent>
 		        <h3>{props.user.name}</h3>
 		        <p>Answered Questions: {Object.keys(props.user.answers).length}</p>
