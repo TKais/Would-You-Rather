552d7baa9192fcaabb9750b74d2ff3a7ab60303b
TKais <TiffanyTKaiser@gmail.com>
2018-09-13 09:14:08 -0700

    Add a reducer for the loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { currentUser } from './currentUser';
 import { users } from './users';
 import { questions } from './questions';
+import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
 	currentUser,
 	users,
 	questions,
+	loadingBar: loadingBarReducer,
 });
