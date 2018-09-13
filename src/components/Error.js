9118ba50051f32c71798c5ccdf46422894c2ecb3
TKais <TiffanyTKaiser@gmail.com>
2018-09-12 20:46:03 -0700

    refactor: Add error classes

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../assets/css/error.css';
 
 function Error(props) {
 	return (
-		<div>
+		<div className="error">
 		    { props.location.state.errorType === '401' ?
-		        <div>
+		        <div className="error__content">
 				    <h1>{ props.location.state.errorType }: Unauthorized</h1>
-				    <p>Please <NavLink to='/' exact >Sign In</NavLink> to continue.</p>
+				    <p>Please <NavLink className='error__content-link' to='/' exact >Sign In</NavLink> to continue.</p>
 			    </div>
 			    :
 			    <div>
