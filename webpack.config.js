a785c48c4d8a1fec8f151709fa4500492d72cf51
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-05-03 20:40:14 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // publicPath: '',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js|.jsx/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+      {
+        test: /\.(png|jpeg|svg)$/i,
+        loader: 'file-loader'
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
