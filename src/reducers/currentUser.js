5eb5884056d209f094e4f274bb4d6e3c91aac09d
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2021-05-05 13:32:34 -0700

    Fix spacing

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -3,15 +3,15 @@ import { SET_CURRENT_USER } from '../actions/currentUser';
 export function currentUser(state = null, action) {
   switch(action.type) {
   case SET_CURRENT_USER:
-		    if( action.id !== null ) {
-		    	return {
-		            ...state,
-		            ...action.id
-		    	};
-		    } else {
-		    	return null;
-		    }
+		if( action.id !== null ) {
+			return {
+				...state,
+				...action.id
+			}
+		} else {
+			return null;
+		}
   default:
-		    return state;
+		return state;
   }
 }
